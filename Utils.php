b1fe820c1146f03bf4990c0e9a7d094dd885997d
Vilmos Ioo <cool.villi@gmail.com>
2013-12-28 19:07:00 +0000

    Added function to print post metadata.

--- Utils.php
+++ Utils.php
@@ -108,6 +108,18 @@ class Utils{
 	}
 
 	/* 
+	* Prints out the current post metadata
+	* 
+	*/
+	static function post_meta(){
+		?>
+		<aside class='aside meta'>
+			Posted on <?php the_time(get_option('date_format')); ?> in <?php the_category(', '); ?> <?php the_tags(' &#8226; Talking about ', ', '); ?> &#8226; <a href='#comments'><?php comments_number('No Comments :(', 'One Comment', '% Comments' ); ?></a> &#8226; <a title="Permalink to <?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>">Permalink</a> 
+		</aside>
+		<?php
+	}
+
+	/* 
 	* Get a custom length excerpt
 	*/
 	static function custom_excerpt($s, $length){
